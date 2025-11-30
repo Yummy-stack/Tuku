0fb0e8bb0e2b63de0cfebca77c9ee11372e56491
Yumy <2512061959@qq,com>
2025-11-30 16:50:55 +0800

    origin version

---
+++ src/main/java/com/tuku/TuKuApplication.java
@@ -0,0 +1,13 @@
+package com.tuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuKuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuKuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuku/TuKuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuKuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
