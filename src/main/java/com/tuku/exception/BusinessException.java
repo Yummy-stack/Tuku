a1aa47cb0afcca09ab2c9e714ccb69d84371b041
Yumy <2512061959@qq,com>
2025-11-30 17:06:29 +0800

    项目结构初始化

---
+++ src/main/java/com/tuku/exception/BusinessException.java
@@ -0,0 +1,28 @@
+package com.tuku.exception;
+
+import com.tuku.model.enums.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private static final long serialVersionUID = 1690627443554021401L;
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+}
---
+++ src/main/java/com/tuku/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.tuku.exception;
+
+import com.tuku.common.BaseResponse;
+import com.tuku.model.enums.error.ErrorCode;
+import com.tuku.utils.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("Controller出现了BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("Controller出现了RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
